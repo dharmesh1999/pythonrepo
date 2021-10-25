19b6a4d46c0d5ba6b60ab59e9fad4dbbd02bde8a
dharmeash1999 <dharmeashk4283@gmail.com>
2021-10-25 21:16:44 +0530

    intial commit 1

---
+++ import_csv.py
@@ -0,0 +1,10 @@
+import csv
+with open('ipl_league.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        if row[1]=="city":
+            continue
+        elif((row[2] == "bat") and (row[8]==row[9])) :
+            print(row)
+
+    
